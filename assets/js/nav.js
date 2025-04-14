57737ef309fb45cd0ee4017e35498527968c376a
Yoan <yoandiny14@gmail.com>
2025-04-14 08:33:20 +0300

    Optimisation de l'affichage du menu de navigation

---
+++ assets/js/nav.js
@@ -0,0 +1,23 @@
+const menu = document.getElementById("navMenu");
+
+menu.innerHTML = `
+           <div class="navbar flex-btw">
+            <link href='https://fonts.googleapis.com/css?family=Roboto Mono' rel='stylesheet'>
+           <link rel="stylesheet" href="assets/css/nav.css">
+            <div class="logo">
+                <i class="fa-solid fa-terminal"></i>
+                <h1>Keygo</h1>
+            </div>
+            <nav class="flex-btw">
+                <ul class="flex-btw">
+                    <li><a href="index.html">Test</a></li>
+                    <li><a href="about.html">About</a></li>
+                </ul>
+
+                <ul class="flex-btw">
+                    <li><a href="#notification.html"><i title="notification" class="fa-solid fa-bell"></i></a></li>
+                    <li><a href="profil.html"><i title="profil" class="fa-regular fa-user"></i></a></li>
+                </ul>
+            </nav>
+        </div>
+`;
