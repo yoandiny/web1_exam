d1cc474d1c00fca0bb27c3185ca8c5f3c15769e3
Yoan <yoandiny14@gmail.com>
2025-04-17 23:07:02 +0300

    Ajout du menu d'information principal du profil utilisateur

--- README.md
+++ README.md
@@ -7,6 +7,17 @@ Yoan - STD24005 | Rindra - STD24018
 KeyGo is a typing website where you can test your skills through various game modes.
 
 ### Available Modes
+- Bounce mod
+- rCaSe
+- Shake
+- Reverse
+- Flash
+
+### Music player
+You have the possibility to play those background musics while you are typing :
+- Metal
+- Heavy
+- Lead
 
 
 Have fun 🔥
--- assets/js/profil.js
+++ assets/js/profil.js
@@ -1,5 +1,13 @@
 const isConnected = localStorage.getItem("isConnected");
 const disconnectButton = document.getElementById('disconnect-button');
+const typingHistory =JSON.parse(localStorage.getItem("typingHistory"))
+const username = localStorage.getItem("username");
+const usernameField = document.querySelector('.user-name');
+const id = localStorage.getItem("id");
+const idField = document.querySelector('.user-id');
+
+const passwordField = document.getElementById('password');
+
 
 if (!isConnected || isConnected === "false"){
     window.location.href = "login.html";}
@@ -8,3 +16,14 @@ const logout = () => {
     localStorage.setItem("isConnected", "false");
     window.location.href = "login.html";
 }
+
+usernameField.innerText = username;
+idField.innerText = id;
+
+document.getElementById('email').placeholder = localStorage.getItem('mail')
+
+
+
+
+
+
