8d3c5e26a9c59aa06f74da2e0867f1759b0deaec
Yoan <yoandiny14@gmail.com>
2025-04-18 09:48:40 +0300

    Ajout du système de modification des données du profil

--- assets/js/profil.js
+++ assets/js/profil.js
@@ -1,6 +1,7 @@
 const isConnected = localStorage.getItem("isConnected");
 const disconnectButton = document.getElementById('disconnect-button');
 const typingHistory =JSON.parse(localStorage.getItem("typingHistory"))
+const profileForm = document.querySelector('.profile-form');
 const username = localStorage.getItem("username");
 const usernameField = document.querySelector('.user-name');
 const id = localStorage.getItem("id");
@@ -21,6 +22,63 @@ usernameField.innerText = username;
 idField.innerText = id;
 
 document.getElementById('email').placeholder = localStorage.getItem('mail')
+
+const handleUpdate = () => {
+    const updateButton = document.getElementById('update-button');
+    updateButton.innerText= '...';
+    
+    setTimeout(() => {
+        const mail = document.getElementById('email').value;
+        const stockedMail = localStorage.getItem('mail');
+        const password = document.getElementById('password').value;
+        const stockedPassword = localStorage.getItem('password');
+        if(mail != stockedMail){
+            if(mail == null || mail == ''){
+                localStorage.setItem('mail', stockedMail);
+                
+            }else{
+                if(mail && mail.includes('@') && mail.includes('.')){
+                    localStorage.setItem('mail', mail);
+                    
+                }else{
+                    alert('Veuillez rentrer une adresse mail valide !')
+                }
+
+            }
+
+        }else{
+            localStorage.setItem('mail', stockedMail);
+        }
+
+      
+
+        if(password != stockedPassword){
+            if(password == null || password == ''){
+                localStorage.setItem('password', stockedPassword);
+            }else{
+                if(password.length >= 6){
+                    localStorage.setItem('password', password);
+                }else{
+                    alert("Votre mot de passe doit contenir au moins 6 caractères !")
+                }
+                
+            }
+        }else{
+            localStorage.setItem('password', stockedPassword);
+        }
+
+        window.location.reload();
+
+    }, 2000);
+    
+}
+
+
+
+profileForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    handleregister();
+});
 
 
 
